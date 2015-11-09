b4fb34d8a8bbc0f9fb1a4ecc258bebe27978d4bc
Alexander Bau <alexander.bau@emtec.de>
2015-11-09 19:12:39 +0100

    improvements

--- bash/$help.js
+++ bash/$help.js
@@ -1,42 +1,57 @@
 /*global module*/
 /*global require*/
 
-"use strict";
+(function () {
+  "use strict";
 
-module.exports = function (args, stdout, stderr, tab) {
+  var sprintf = require("sprintf-js").sprintf;
+
+  module.exports = function (args, stdout, stderr, tab) {
 
     var bash = require('include-all')({
-        dirname: __dirname,
-        filter: /(.*)\.js$/
+      dirname: __dirname,
+      filter: /(.*)\.js$/
     });
 
-    stdout("HELP :O");
-    stdout("==========================");
-    stdout("custom commands:");
-    stdout("\n");
+    stdout("[34mcustom commands:");
 
     var line = "   ├── ";
     var lineEnd = "   └── ";
 
     for (var command in bash) {
-        if (bash.hasOwnProperty(command)) {
-            var script = bash[command];
-            stdout(" • " + command);
-            if (script.help) {
-                if (script.help instanceof Array) {
-                    for (var i = 0; i < script.help.length; i++) {
-                        var helpLine = script.help[i];
-                        stdout((i + 1 < script.help.length ? line : lineEnd) + helpLine);
-                    }
-                } else {
-                    stdout(script.help);
-                }
+      if (bash.hasOwnProperty(command)) {
+        var script = bash[command];
+        stdout(" • " + command);
+        if (script.help) {
+          if (script.help instanceof Array) {
+            for (var i = 0; i < script.help.length; i++) {
+              var helpLine = script.help[i];
+              stdout((i + 1 < script.help.length ? line : lineEnd) + helpLine);
             }
+          } else {
+            stdout(script.help);
+          }
         }
+      }
     }
-};
 
-var sprintf = require("sprintf-js").sprintf;
-module.exports.help = [
+    stdout("\n");
+    stdout("[34mgenereal stuff");
+    stdout(" • " + "you can use your Mouse completely free as long as you hold down [CTRL]");
+
+    stdout("\n");
+    stdout("[34mcontrolls");
+    stdout(" • " + sprintf("%-30s %s", "[alt]  + [left]", "previous tab"));
+    stdout(" • " + sprintf("%-30s %s", "[alt]  + [right]", "next tab"));
+    stdout(" • " + sprintf("%-30s %s", "[ctrl] + [d]", "close tab"));
+    stdout(" • " + sprintf("%-30s %s", "[ctrl] + [c]", "kills process of active tab"));
+    stdout(" • " + sprintf("%-30s %s", "[ctrl] hold down", "releases the cursor (free mouse)"));
+    stdout(" • " + sprintf("%-30s %s", "[ctrl] up", "forces to focus the input"));
+    stdout(" • " + sprintf("%-30s %s", "[tab]", "auto-complete - resolves paths based on CWD"));
+    stdout(" • " + sprintf("%-30s %s", "[tab]  double tab", "executes `ls` command"));
+  };
+
+  module.exports.help = [
     sprintf("%-30s %s", "", "show this actual fucking help")
-];
+  ];
+})();
